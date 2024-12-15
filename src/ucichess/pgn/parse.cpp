5d00adc2d3b40b629f517d09d334de844dbf8240
Nevroz Arslan <nevrozarslan@gmail.com>
2024-12-15 05:21:41 +0300

    add more insert lines

--- src/ucichess/pgn/parse.cpp
+++ src/ucichess/pgn/parse.cpp
@@ -1,7 +1,44 @@
 #include "parse.hpp"
+#include <fmt/format.h>
+#include <numeric>
+#include <string_view>
 #include <utility>
 
 namespace pgn2sqlite {
+
+  /*! note: delimiter cannot contain NUL characters
+ */
+  template <typename Range, typename Value = typename Range::value_type>
+  std::string Join(Range const& elements, const char* const delimiter) {
+    std::ostringstream os;
+    auto b = begin(elements), e = end(elements);
+
+    if(b != e) {
+      std::copy(b, prev(e), std::ostream_iterator<Value>(os, delimiter));
+      b = prev(e);
+    }
+    if(b != e) {
+      os << *b;
+    }
+
+    return os.str();
+  }
+
+  /*! note: imput is assumed to not contain NUL characters
+ */
+  template <typename Input, typename Output, typename Value = typename Output::value_type>
+  void Split(char delimiter, Output& output, Input const& input) {
+    using namespace std;
+    for(auto cur = begin(input), beg = cur;; ++cur) {
+      if(cur == end(input) || *cur == delimiter || !*cur) {
+        output.insert(output.end(), Value(beg, cur));
+        if(cur == end(input) || !*cur)
+          break;
+        else
+          beg = next(cur);
+      }
+    }
+  }
 
   Parser::Parser()
       : db{std::make_unique<pgn2sqlite::pgndb>("abdo.db")} {
@@ -41,13 +78,7 @@ namespace pgn2sqlite {
     m_counter.increment_if_not_zero();
 
     std::string event, site, date, eco, player1, player2, result;
-    auto moves_string =
-        std::accumulate(alist.begin(),
-                        alist.end(),
-                        std::string(),
-                        [](const std::string& a, const std::string& b) -> std::string {
-                          return a + (a.length() > 0 ? "," : "") + b;
-                        });
+    std::string moves_string = Join(moves, " ");
 
     for(auto el : headers) {
       if(el.first == "Event") {
@@ -72,7 +103,8 @@ namespace pgn2sqlite {
         player2 = el.second;
       }
     }
-    auto query = fmt::format("insert into pgn values ({},{},{},{},{},{},{},{},{} ) ",
+
+    auto query = fmt::format("insert into pgn values (NULL, {},{},{},{},{},{},{},{} ) ",
                              event,
                              site,
                              date,
@@ -81,6 +113,13 @@ namespace pgn2sqlite {
                              player2,
                              result,
                              moves_string);
+    db->db.exec(query);
+
+    SQLite::Statement query2(db->db, "SELECT * FROM pgn");
+
+    while(query2.executeStep()) {
+      fmt::print("{} {} \n", query2.getColumn(1).getInt(), query2.getColumn(2).getInt());
+    }
 
     fmt::print("number of pgn  files: {} \n", m_counter.read() - 1);
   }
