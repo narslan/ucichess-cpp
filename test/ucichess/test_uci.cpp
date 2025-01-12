32f2d94492572364fb0e65892f55887cb98b96f8
Nevroz Arslan <nevrozarslan@gmail.com>
2025-01-12 16:55:32 +0300

    new test file

---
+++ test/ucichess/test_uci.cpp
@@ -0,0 +1,11 @@
+#include "../../src/ucichess/uci/engine.hpp"
+#include <fmt/core.h>
+
+const engine_a = "patricia";
+const engine_a = "zahak";
+const engine_a = "stockfish";
+
+int main() {
+
+  ucichess::ChessEngine ce { }
+}
